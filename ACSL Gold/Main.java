d9de0ef8e0d31e03f31a91b85f5ccb0050215372
tuwu <73315373+Tuwuppy@users.noreply.github.com>
2024-12-06 23:13:47 -0500

    Create Main.java

---
+++ ACSL Gold/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        Gold sell = new Gold(1675.38);
+        sell.getValue("240, GN, 24, 20");
+        sell.getValue("6, GM, 12, 30");
+        sell.getValue("25, PN, 18, 36");
+        sell.getValue("3.5, ON, 6, 25");
+        sell.getValue("8, TR, 22, 33");
+    }
+}
